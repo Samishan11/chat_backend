280f3c9a1fe7f14316d309ded110b0aae4730004
Samishan11 <samishanthapa0@gmail.com>
2023-08-25 20:04:25 +0545

    changes

--- src/connection/database.ts
+++ src/connection/database.ts
@@ -3,12 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const DB_URI: string | undefined = process.env.DB_URI;
-console.log(DB_URI);
 export const connectDB = async (): Promise<void> => {
   try {
     if (!DB_URI) return;
-    await mongoose.connect(DB_URI).then(() => {
-    });
+    await mongoose.connect(DB_URI).then(() => {});
   } catch (error: any) {
     console.log(error.message);
   }
