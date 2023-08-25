31ddc4c2f5a92979146d02447964f4b7b3e8a42e
Samishan11 <samishanthapa0@gmail.com>
2023-08-25 17:48:02 +0545

    changes

--- src/connection/database.ts
+++ src/connection/database.ts
@@ -8,7 +8,6 @@ export const connectDB = async (): Promise<void> => {
   try {
     if (!DB_URI) return;
     await mongoose.connect(DB_URI).then(() => {
-      console.log("Database connected");
     });
   } catch (error: any) {
     console.log(error.message);
